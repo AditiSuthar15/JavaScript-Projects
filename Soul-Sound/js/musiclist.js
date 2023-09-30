6c50e2aae6bb106e668531843c173b37ccb79881
Aditi Suthar <121705159+AditiSuthar15@users.noreply.github.com>
2023-09-30 23:17:26 +0530

    Add files via upload

---
+++ Soul-Sound/js/musiclist.js
@@ -0,0 +1,44 @@
+let allMusic = [
+    {
+        name: "Kabhi Kabhi Aditi",
+        artist: "Rashid Ali",
+        img: "music-1",
+        src: "music-1"
+
+    },
+    {
+        name: "Bakhuda",
+        artist: "Pritam, Atif Aslam",
+        img: "music-2",
+        src: "music-2"
+
+    },
+    {
+        name: "Zara Sa",
+        artist: "Pritam, KK",
+        img: "music-3",
+        src: "music-3"
+
+    },
+    {
+        name: "Dandelions",
+        artist: "Ruth B",
+        img: "music-4",
+        src: "music-4"
+
+    },
+    {
+        name: "Until I Found You ",
+        artist: "Stephen Sanchez",
+        img: "music-5",
+        src: "music-5"
+
+    },
+    {
+        name: "Past-Lives",
+        artist: "Borns",
+        img: "music-6",
+        src: "music-6"
+
+    },
+]
